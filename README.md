f0bb28177807ab8fa679c557b0827f696a6d3465
dicksonaj <79535800+dicksonaj@users.noreply.github.com>
2022-04-06 00:15:35 +0100

    Frequency Analysis

---
+++ README.md
@@ -0,0 +1,43 @@
+TOPIC: FREQUENCY ANALYSIS APPLICATION
+
+DESCRIPTION: 
+Frequency analysis is based on the fact that, in any given stretch of written language, certain letters and combinations of letters occur with varying frequencies. This application help crypto analyst to make frequency analysis on cipher text.
+
+This Application has no GUI, it runs in a command line and takes one arguments (cipher.txt) as input.
+
+After application run it analyze cipher.txt and give statistic of chars at the same screen.
+it then displays an Option:
+1) Take replace rule
+2) Exit
+
+If option 1 will be typed then system get replacement rule for analysis. Format of rule looks like CSV like input for example (M:a,A:b,N:c,U:d,S:e,C:f, (So on...))
+
+Input format description:
+1. input pairs should be separated by comma ","
+2. Cipher Text Char:Plain Text Char
+
+After input given according to given input all cipher text chars will be replaced by given rule. So 
+M replaced as a , A replaced by b and so on..
+
+LANGUAGE
+This program is written in Python Language
+
+STEPS
+1. PASS an arguement of file_name to the program file
+2. The application READS the content of the file and save it in a variable
+3. The app ANALYSE the frequency of each letter in the Cipher Text and Return the Statistic
+4. The app GET user input for the Replacement Rule
+5. The app VERIFY the replacement rule to ensure it follows the format for the replacement rule
+6. The app EXTRACTS the Cipher Text Char and Plain Text Char pairs from the replacement rule
+7. The CipherText is DECRYPTED following the replacement rule
+8. The result of the Analysis and Plain Text is DISPLAYED to the screen.
+
+SETUP
+
+1. Install Python 3.9 on your Machine
+2. Creat a Folder
+3. Save the files analysis.py and .txt file in the same folder created
+4. Open your command line interface by searchin cmd, or Windows + R, then search cmd
+5. cd to the Folder location path
+6. Run command 'python -m analysis [file_name.txt]' to RUN app
+
---
+++ analysis.py
@@ -0,0 +1,176 @@
+from operator import countOf
+import re
+import sys
+from typing import List, Dict
+
+def main() -> None:
+    """ Entrypoint of program run as module """
+
+    file_name = read_args() # Save filename from Arguement passed
+    try:
+        content = read_file(file_name)
+        print("Cipher Text :\n")
+        print(content)
+
+        """ Analyse the Cipher Text and Give Statistic of Char """
+        analysis(content)
+
+        flag = True
+        while flag:
+            try:
+                """ Display an Option Entry to Perform Action Based on User Option Input """
+                print("\n\nOption: \n")
+                print("1) Take replace rule\n")
+                print("2) Exit\n")
+
+                optionValue = int(input("Option> "))
+
+                if optionValue == 1:
+                    format_info() # Display the Replacement Rule Format Info
+
+                    """ Verify if replacement rule format is valid"""
+                    format = True
+                    while format:
+                        replacementRule = input("Enter Replacement Rule-> ")
+                        
+                        pattern = "^([A-Z](:)[a-z](,)?)+"
+                        charMapping = {}
+                        Arr = []
+                        if(re.fullmatch(pattern,replacementRule)):
+                            ruleArr = re.split(",",replacementRule) # Create a list of Rule Pairs by removing the comma
+                            for rule in ruleArr:
+                                ruleSplit = re.split(":",rule) # Get Cipher Text Char and Plain Text Char from the Rule Pairs
+                                # Dictionary to check if Cipher Text Char contains all alphabet
+                                for letter in ruleSplit:
+                                    Arr.append(letter) # Append each letter in the rule pair to check for duplicate value
+                                for char in Arr:
+                                    if countOf(Arr, char) > 1:
+                                        print("\t*** Invalid Replacement Rule ***\n")
+                                        print("%s appears more than once"%(char))
+                                        format_info()
+                                        format = True
+                                        break
+                                    else:
+                                        # return the mapping after succesful verification
+                                        charMapping[ruleSplit[0]] = ruleSplit[1]
+                                        format = False
+                            
+                        else:
+                            print("\t*** Invalid Replacement Rule ***")
+                            format_info()
+                            format = True
+                        
+                        
+                        """ Check if All Characters have been  Visited in The Replacement Rule"""
+                        letterVisited = ""
+                        for key in charMapping:
+                            letterVisited += key
+                        
+                        if check_rule_ispangram(letterVisited) == False:
+                            proceedFlag = True
+                            while proceedFlag:
+                                try:
+                                    print("\t*** The Cipher Text Char in Replacement Rule does not contain all Alphabet ***")
+                                    print("Option: \n1 to continue with Replacement Rule\n2 to Enter a New Replacement Rule\n3 to exit program ***")
+                                    proceed = int(input("Proceed> "))
+                                    if proceed == 1:
+                                        # Action on replacement rule Begin Here
+                                        plainTextContent = ""
+                                        for cipherLetter in content:
+                                            if cipherLetter.isspace():
+                                                plainTextContent += cipherLetter
+                                            elif not cipherLetter.isalnum():
+                                                plainTextContent += cipherLetter
+                                            else:
+                                                count = 0
+                                                for key in charMapping:
+                                                    plainTextChar = charMapping[key]
+                                                    
+                                                    if cipherLetter == key:
+                                                        plainTextContent += plainTextChar
+                                                        break
+                                                    else:
+                                                        count += 1
+                                                if count == len(charMapping):
+                                                    plainTextContent += cipherLetter
+                                                    count = 0
+                                        """ After Verification of Replacement Rule Perform Action on Cipher Text"""
+                                        print("\nCipher Text :\n")
+                                        print(content)
+                                        analysis(content)
+                                        print("\n\nPlain Text: \n\n")
+                                        # Plain Text Result of Replacement rule is displayed here
+                                        print(plainTextContent)
+                                        # Action on replacement rule Ends Here
+                                        # ***********************
+                                        proceedFlag = False
+                                    elif proceed == 2:
+                                        proceedFlag = False
+                                        format = True
+
+                                    elif proceed == 3:
+                                        proceedFlag = False
+                                        flag = False
+                                        exit()
+                                except ValueError:
+                                    print("\n\t*** ValueError: Invalid Input, Integer Required ***\n\n")
+                                    proceedFlag = True
+                            
+                elif optionValue == 2:
+                    flag = False
+                    exit() 
+
+                else:
+                    print("\n\t**** Invalid Option Selected ****\n")
+                    flag = True
+
+            except ValueError as ve:
+                print("\n\t*** ValueError: Invalid Input, Integer Required ***")
+                flag = True
+
+    except FileNotFoundError:
+        msg = "*** Sorry, the file " + file_name + " does not exist.***\n*** Ensure the file is saved in the same folder as the program file ***"
+        print(msg)
+
+
+
+def read_args():
+    if len(sys.argv) != 2:
+        print("Usage: python -m analysis [filename]")
+        exit()
+    return sys.argv[1]
+
+def read_file(file_name: str):
+    with open(file_name,"r") as file_handle:
+        return file_handle.read()
+
+def analysis(contents: str):
+    letter_analysis = {}
+    for letter in contents:
+        if(letter.isspace() or letter.isalnum() == False): 
+            pass
+        else:
+            letter_analysis[letter] = contents.count(letter)
+    print("\nAnalysis :\n")
+    for key,value in letter_analysis.items():
+        result = key + "->" + str(value) + ","
+        print(result,end=' ')
+
+def format_info():
+    print("\n******** REPLACEMENT RULE FORMAT *********\n")
+    print("Cipher Text Char:Plain Text Char. Input pairs should be seperated by a comma")
+    print("Cipher Text Char and Plain Text Char should not occur more than once")
+    print("Format: M:a,B:c,C:d .....")
+    print("\n******** REPLACEMENT RULE FORMAT *********\n")
+
+""" Function to check if Cipher Text in Replacement Rule contains all alphabet"""
+def check_rule_ispangram(str):
+    alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    for char in alphabet:
+        if char not in str:
+            return False
+    return True
+
+if __name__ == "__main__":
+    main()
+
